7b5361cbac51a1680cf7e0c2870d4398e052ed24
anukriti.saxena <anukriti.saxena@acuverconsulting.com>
2025-08-16 15:19:57 +0530

    Delete dummy code structure

--- demo/bootcamp/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package bootcamp.src.main.java.com.example.demo;
-
-
-import com.example.demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
